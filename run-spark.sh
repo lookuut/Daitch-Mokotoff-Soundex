5c6c8201ba82833f1a833b387814a2e3675c80bd
Lookuut Struchkov <lookuut@gmail.com>
2018-02-09 07:57:57 +0300

    gitignore

--- run-spark.sh
+++ run-spark.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 sbt package
-sh "${SPARK_HOME}"/bin/spark-submit --class "app.spark.PersonNameIdentifier"  target/scala-2.11/ru-translit-metaphone_2.11-0.1.0.jar /home/lookuut/Projects/person-name-identifier/resource/persons.csv
+sh "${SPARK_HOME}"/bin/spark-submit --class "app.spark.PersonNameIdentifier"  target/scala-2.11/ru-translit-metaphone_2.11-0.1.0.jar /resource/persons.csv
